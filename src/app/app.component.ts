2942208819f7f9181260addef2e1a6bc7e2f12b1
Bogdan <bogdanstefanjuk@gmail.com>
2020-02-29 13:49:45 +0200

    Add template variable for accessing app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle', {static: false}) appTitleElement: ElementRef;
   title = 'shop';
+
+  ngAfterViewInit(): void {
+    this.appTitleElement.nativeElement.textContent = 'Apple shop';
+  }
 }
