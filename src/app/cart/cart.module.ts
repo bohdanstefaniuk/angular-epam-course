61df115cb866f2e0037a38eeff6a55173e6ce09a
Bogdan <bogdanstefanjuk@gmail.com>
2020-02-29 13:50:09 +0200

    Add highlight directive for cart item

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 import { CartListComponent } from './components/cart-list/cart-list.component';
 import { CartService } from './services/cart.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CartItemComponent, CartListComponent],
   providers: [CartService],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [CartListComponent]
 })
